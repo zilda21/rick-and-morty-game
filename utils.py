0a39120f7233238b9d77534f9d5a68584dd695a5
zilda21 <164346657+zilda21@users.noreply.github.com>
2025-10-02 16:36:56 +0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,4 +1,4 @@
 import hmac, hashlib, secrets
 def generate_key(): return secrets.token_hex(16)
 def hmac_sha256(key, message): return hmac.new(key.encode(), str(message).encode(), hashlib.sha256).hexdigest()
-def random_choice_secure(limit): return secrets.randbelow(limit)
+def secure_random  (limit): return secrets.randbelow(limit)
