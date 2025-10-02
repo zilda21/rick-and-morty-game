c9aaeeac4da18bc1e607778e357e739c948e762f
zilda21 <164346657+zilda21@users.noreply.github.com>
2025-10-02 16:34:40 +0300

    Update classic_morty.py

--- classic_morty.py
+++ classic_morty.py
@@ -3,6 +3,6 @@ from morty_base import MortyBase
 # ClassicMorty behaves same as base, but could be extended...
 
 class ClassicMorty(MortyBase):
- print("Classic Morty is playing...")
-
-    pass
+ def __init__(self):
+        super().__init__()
+        print("Classic Morty is playing...")
