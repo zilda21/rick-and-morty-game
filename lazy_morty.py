832f8cef307e4f2d35b40a885c0f4c61e0f8ef39
zilda21 <164346657+zilda21@users.noreply.github.com>
2025-10-02 19:03:08 +0300

    Update lazy_morty.py

--- lazy_morty.py
+++ lazy_morty.py
@@ -1,7 +1,8 @@
+# lazy_morty.py
 from morty_base import MortyBase
 
 class LazyMorty(MortyBase):
-      def __init__(self):
+    def __init__(self):
         super().__init__()
         print("Lazy Morty yawns... let's see what happens.")
-    #instead you can add pass both inputs works for me
+    # uses base probabilities too
